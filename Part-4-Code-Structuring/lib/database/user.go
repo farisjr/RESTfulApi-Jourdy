e3383d4e75d6080dfd6ba104ec94d20e2384614c
FarisJR <55522910+farisjr@users.noreply.github.com>
2021-07-23 05:47:01 +0700

    Update user.go

--- Part-4-Code-Structuring/lib/database/user.go
+++ Part-4-Code-Structuring/lib/database/user.go
@@ -1,1 +1,72 @@
 package database
+
+import (
+	"farisjr/project/config"
+	"farisjr/project/middlewares"
+	"farisjr/project/models"
+)
+
+func GetUsers() (interface{}, error) {
+	var users []models.User
+	if err := config.DB.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func GetUserById(id int) (models.User, error) {
+	var user models.User
+	if err := config.DB.Find(&user, "id=?", id).Error; err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func CreateUser(create_user models.User) (interface{}, error) {
+	if err := config.DB.Save(&create_user).Error; err != nil {
+		return nil, err
+	}
+	return create_user, nil
+}
+
+func DeleteUserById(delete_user models.User) (interface{}, error) {
+	if err := config.DB.Delete(&delete_user).Error; err != nil {
+		return nil, err
+	}
+	return delete_user, nil
+}
+
+func UpdateUserById(update_user models.User) (interface{}, error) {
+
+	if err := config.DB.Save(&update_user).Error; err != nil {
+		return nil, err
+	}
+	return update_user, nil
+}
+
+func GetDetailUsers(userId int) (interface{}, error) {
+	var user models.User
+	if err := config.DB.Find(&user, userId).Error; err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func LoginUsers(user *models.User) (interface{}, error) {
+	var err error
+	if err = config.DB.Where("email = ? AND password = ?", user.Email, user.Password).First(user).Error; err != nil {
+		return nil, err
+	}
+
+	user.Token, err = middlewares.CreateToken(int(user.ID))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := config.DB.Save(user).Error; err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
