43df65cc16775273906eed7a81e91a57c2fcfb35
FarisJR <55522910+farisjr@users.noreply.github.com>
2021-07-23 05:48:25 +0700

    Update book.go

--- Part-4-Code-Structuring/models/book.go
+++ Part-4-Code-Structuring/models/book.go
@@ -1,1 +1,10 @@
 package models
+
+import "gorm.io/gorm"
+
+type Book struct {
+	gorm.Model
+	Name      string `json:"name" form:"name"`
+	Tittle    string `json:"title" form:"title"`
+	Publisher string `json:"publisher" form:"publisher"`
+}
