9025b9818911edc1b2379b768b135196ea17638a
FarisJR <55522910+farisjr@users.noreply.github.com>
2021-07-23 05:46:18 +0700

    Update userController.go

--- Part-4-Code-Structuring/controllers/userController.go
+++ Part-4-Code-Structuring/controllers/userController.go
@@ -1,1 +1,135 @@
 package controllers
+
+import (
+	"farisjr/project/lib/database"
+	"farisjr/project/models"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo"
+)
+
+//get all users
+func GetUserControllers(c echo.Context) error {
+	users, err := database.GetUsers()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"status": "success",
+		"users":  users,
+	})
+}
+
+// get user by id
+func GetUserByIdController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": "invalid id",
+		})
+	}
+	user, err := database.GetUserById(id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "cannot fetch data",
+		})
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success",
+		"data":    user,
+	})
+}
+
+// create new user
+func CreateUserController(c echo.Context) error {
+	create_user := models.User{}
+	c.Bind(&create_user)
+
+	user, err := database.CreateUser(create_user)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success create new user",
+		"user":    user,
+	})
+}
+
+// delete user by id
+func DeleteUserController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": "invalid id",
+		})
+	}
+	delete_user, err := database.GetUserById(id)
+	c.Bind(&delete_user)
+	userId, err := database.DeleteUserById(delete_user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "cannot delete data",
+		})
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "delete success ",
+		"data":    userId,
+	})
+}
+
+// update user by id
+func UpdateUserController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": "invalid id",
+		})
+	}
+
+	update_user, err := database.GetUserById(id)
+	c.Bind(&update_user)
+	userId, err := database.UpdateUserById(update_user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "cannot update data",
+		})
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success",
+		"data":    userId,
+	})
+}
+
+//Login user
+func LoginUsersController(c echo.Context) error {
+	user := models.User{}
+	c.Bind(&user)
+
+	users, err := database.LoginUsers(&user)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error)
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"status": "login success",
+		"users":  users,
+	})
+}
+
+//Get user detail
+func GetUserDetailController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	users, err := database.GetDetailUsers((id))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"status": "succes",
+		"users":  users,
+	})
+}
